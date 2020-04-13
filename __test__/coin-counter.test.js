6b594936cc889ad1995ce6d3b3c41979195b8576
Jieun Kang <jieunkang101@gmail.com>
2020-04-13 12:40:36 -0700

    add 2nd test and get pass

--- __test__/coin-counter.test.js
+++ __test__/coin-counter.test.js
@@ -1,9 +1,14 @@
-import { CoinCounter }  from './../src/coin-counter';
+import { coinCounter }  from './../src/coin-counter';
 
-describe('CoinCounter', () => {
+describe('coinCounter', () => {
 
   test('should correctly paas in through a number', () => {
-    const userAmount = CoinCounter("test");
+    const userAmount = coinCounter("test");
     expect(userAmount).toEqual("Invalid input");
+  });
+
+  test('should determine the exact amount of change needed in quarters', () => {
+    const userAmount = coinCounter(7.44);
+    expect(userAmount).toEqual(29);
   });
 });
--- src/coin-counter.js
+++ src/coin-counter.js
@@ -1,8 +1,16 @@
-export const CoinCounter = (amount) => {
-  
+export const coinCounter = (amount) => {
+  let coinPurse = {};
+
   if (isNaN(amount)) {
     return "Invalid input";
   }
 
-  
+  if (amount >= .25) {
+    coinPurse.quaters = Math.floor(amount / .25);
+    // amount = Math.fround(amount % .25).toPrecision(2);
+    amount = parseFloat(amount % .25).toPrecision(2);
+    return coinPurse.quaters;
+  }
+
+
 }
