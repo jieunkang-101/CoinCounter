5cf7106bfae7158ba577b83c79a29fe9607dfeea
Jieun Kang <jieunkang101@gmail.com>
2020-04-13 11:52:32 -0700

    add 1st test and get fail

--- __test__/coin-counter.test.js
+++ __test__/coin-counter.test.js
@@ -2,5 +2,8 @@ import { CoinCounter }  from './../src/coin-counter';
 
 describe('CoinCounter', () => {
 
-  
+  test('should correctly paas in through a number', () => {
+    const userAmount = new CoinCounter(7.41);
+    expect(CoinCounter.amount).toEqual(7.41);
+  });
 });
